57450719a7e99acc120b9e6158b360bd7282b45e
aboudaIbrahim <MohamedIbrahim.Abouda@isitc.u-sousse.tn>
2023-02-22 23:22:02 +0100

    commit

--- app/Http/Controllers/Api/TodoController.php
+++ app/Http/Controllers/Api/TodoController.php
@@ -18,43 +18,73 @@ TodoController extends Controller
         $this->middleware('auth:api');
     }
 
-
+    /**
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function index()
     {
         //dd(Route::currentRouteName());
         return $this->todoRepository->index();
     }
 
+    /**
+     * @param TodoRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function store(TodoRequest $request)
     {
         return $this->todoRepository->store($request);
     }
 
+    /**
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function show($id)
     {
         return $this->todoRepository->show($id);
     }
 
+    /**
+     * @param TodoRequest $request
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function update(TodoRequest $request, $id)
     {
         return $this->todoRepository->update($request, $id);
     }
 
+    /**
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function destroy($id)
     {
         return $this->todoRepository->destroy($id);
     }
 
+    /**
+     * @return mixed
+     */
     public function trashedTodos()
     {
         return $this->todoRepository->displayTrashedTodod();
     }
 
+    /**
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function restoreTrashedTodo($id)
     {
         return $this->todoRepository->restoreTrashedTodos($id);
     }
 
+    /**
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function forceDeleteTrashedTodo($id)
     {
         return $this->todoRepository->forceDeletingTrashedTodo($id);
